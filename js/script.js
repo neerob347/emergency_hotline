c5a731b0e03353483e8e05e2714624bf13eab8b5
neerob347 <rxneerob@gmail.com>
2025-08-29 21:40:22 +0600

    added functionalities of js

--- js/script.js
+++ js/script.js
@@ -8,3 +8,43 @@
       heartCounter.innerText = count;
    }
 }
+
+let coins = 100;
+const callButtons = document.getElementsByClassName("callBtn");
+const coinCount = document.getElementById("coinCount");
+const historyList = document.getElementById("historyList");
+const clearBtn = document.getElementById("clearHistory");
+
+coinCount.innerText = coins;
+
+
+for (let i = 0; i < callButtons.length; i++) {
+  callButtons[i].onclick = function () {
+    const card = this.closest(".bg-white");  
+    const title = card.querySelector(".serviceName").innerText;   
+    const info  = card.querySelector(".serviceNumber").innerText; 
+
+    if(coins >= 20){
+      coins -= 20;
+      coinCount.innerText = coins;
+    alert("📞 Calling..." + title + " " + info);
+const leftPart = document.createElement("div");
+leftPart.innerHTML = `<strong>${title}</strong><br>${info}`;
+
+const time = document.createElement("span");
+time.className = "text-sm text-gray-500";
+time.innerText = new Date().toLocaleTimeString();
+
+const li = document.createElement("li");
+li.className = "flex justify-between items-center  py-4";
+li.appendChild(leftPart);
+li.appendChild(time);
+
+historyList.appendChild(li);
+    }
+    else{
+      alert("❌Insufficient Coin")
+    }
+  }
+  
+}
